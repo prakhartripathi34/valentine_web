0c8388c1bcbb571baa232f6f7d6d73110c3f93ff
prakhartripathi34 <hackerninja766@gmail.com>
2024-02-04 22:12:48 +0530

    files_Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react" ;
+import { useState } from "react";
+import "./App.css";
+
+const phrases = {
+  0: "No",
+  1: "Are you sure?",
+  2: "Really?",
+  3: "please",
+  4: " i am  gonna cry",
+  5: "please",
+};
 
 function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 60;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, Object.keys(phrases).length - 1)];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    React.createElement("div", { className: "valentine-container" },
+      yesPressed ? (
+        [
+          React.createElement("img", {
+            alt: "dancing",
+            src: "https://media.tenor.com/rLnhPt31OFUAAAAM/swing-dance-swing-your-hips.gif",
+          }),
+          React.createElement("img", {
+            alt: "kissing",
+            src: "https://media.tenor.com/TtwmH_yQ3XAAAAAM/mewdi-moni.gif",
+          }),
+          React.createElement("div", { className: "text" }, "Yayy!!!!"),
+        ]
+      ) : (
+        [
+          React.createElement("img", {
+            alt: "with heart",
+            src: "https://media.tenor.com/HXa-urHqboYAAAAM/milk-and-mocha-i-love-you.gif",
+          }),
+          React.createElement("div", null, "Will you be my valentine...."),
+          React.createElement("div", null,
+            React.createElement("button", {
+              className: "yesButton",
+              style: { fontSize: yesButtonSize },
+              onClick: () => setYesPressed(true),
+            }, "Yes"),
+            React.createElement("button", { className: "noButton", onClick: handleNoClick },
+              getNoButtonText()
+            ),
+          ),
+        ]
+      ),
+    )
   );
 }
 
